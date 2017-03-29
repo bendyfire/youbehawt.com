2b23845126091b3efb499a24d08503e7518bf605
Bendy Straw <bnjmnstvns@gmail.com>
2017-03-29 17:23:22 -0600

    PUSSSHHHHH DA BUTTT-ON

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var img1 = document.getElementById("clickButton");
+
+img1.addEventListener("mousedown", function buttonClick() {
+	img1.src = "button1.png";
+});
+
+img1.addEventListener("mouseup", function buttonClick() {
+	img1.src = "button2.png";
+});
